d95176795b314d07859d512c0e0fe1f1f0fae1b6
Manvendra Singh Deora <deora0799@gmail.com>
2020-05-17 16:26:50 +0530

    Chest ScriptableObject

---
+++ Tower Defence/Assets/Scripts/Core Game Script/Chest Script/Chest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName ="New Chest",menuName = "Scritable Object/Chest")]
+public class Chest : ScriptableObject
+{
+    public new string name;
+    [Tooltip("Enter Time in Second")]
+    public float _ChestTime;
+    float _TimeLeft;
+    public string _Discription;
+    public Sprite _Artwork;
+
+    private void Awake()
+    {
+        Debug.Log("Function");
+    }
+
+    private void OnEnable()
+    {
+        _TimeLeft = _ChestTime;
+        Debug.Log(_TimeLeft+"OnEnable");
+    }
+
+    public string CountDownTimer()
+    {
+        if(_TimeLeft <= 0)
+        {
+            ChestOpen();
+             
+            return "00:00";
+        }
+
+        int minutes =(int)_TimeLeft / 60;
+        int second = (int)_TimeLeft % 60;
+        _TimeLeft -= Time.deltaTime;
+
+        return minutes.ToString() + ":" + second.ToString();
+        
+    }
+
+
+    public void ChestOpen()
+    {
+        Debug.Log("Open Chest Function");
+    }
+
+}
+
+
+//[System.Serializable]
+//class TimeConvertor
+//{
+//    public readonly string _Time;
+
+//    public TimeConvertor(int time)
+//    {
+//        TimeSpan timeSpan = TimeSpan.FromSeconds(time);
+//        this._Time = timeSpan.ToString("mm':'ss");
+//    }
+
+//}
--- Tower Defence/Assets/Scripts/Core Game Script/GameManager.cs
+++ Tower Defence/Assets/Scripts/Core Game Script/GameManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Chest _ChestObject;
+    string timer;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       timer =_ChestObject.CountDownTimer();
+        Debug.Log(timer);
     }
 }
