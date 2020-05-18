213ab7ef83a9a58229c20e2003076e029ea1c342
Manvendra Singh Deora <deora0799@gmail.com>
2020-05-18 15:56:26 +0530

    buliding System
    created sriptable Object and Buliding Upgrade System

--- Tower Defence/Assets/Scripts/Core Game Script/GameManager.cs
+++ Tower Defence/Assets/Scripts/Core Game Script/GameManager.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Chest _ChestObject;
+    public BulidingObject _BulidingObject;
     string timer;
+    public bool upgrade;
     // Start is called before the first frame update
     void Start()
     {
+
     }
     // Update is called once per frame
     void Update()
     {
-        timer = _ChestObject.CountDownTimer();
-        Debug.Log(timer);
+        if (upgrade)
+        {
+            _BulidingObject.UpgradeSystem(true);
+            upgrade = false;
+        }
+        Debug.Log(_BulidingObject._CurrentStroageCapacity);
+        //timer = _ChestObject.CountDownTimer();
+        //Debug.Log(timer);
 
     }
 }
