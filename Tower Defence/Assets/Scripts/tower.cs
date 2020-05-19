6d45d8eca9d2c27d40291d2f900083632379cafe
kshitizvyas <42069+kshitizvyas@users.noreply.github.com>
2020-05-19 18:05:47 +0530

    tweenpopup

--- Tower Defence/Assets/Scripts/tower.cs
+++ Tower Defence/Assets/Scripts/tower.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class tower : MonoBehaviour
 {
+    public Transform popuptransform;
+    public GameObject troopspopup;
+
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void trooppops()
+    {
+        Instantiate(troopspopup, popuptransform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
---
+++ Tower Defence/Assets/Tweening.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweening : MonoBehaviour
+{
+    public LeanTweenType outType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.scale(gameObject, new Vector3(1.5f, 1f, 1f), 0.3f).setEase(outType).setDestroyOnComplete(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
